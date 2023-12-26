02f6590fb83173f3b76cc7ae5b32c7d842bdad78
Aadish Sheth <131483259+aadish2210@users.noreply.github.com>
2023-12-27 03:40:12 +0530

    updated backend

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -1,35 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const Replicate = require('replicate');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
+
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
+
+const MODEL_NAME = "models/chat-bison-001";
+const API_KEY = "AIzaSyBTnuWUwI2vlaSZioRgs1PsAvwj9Z6Rq1M";
+
+const client = new DiscussServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+
 const port = 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/chatbot', async (req, res) => {
+app.post("/api/chatbot", async (req, res) => {
   const userMessage = req.body.message;
+  const context = "";
+  const examples = [];
+  const messages = [];
 
-  const replicate = new Replicate({
-    auth: "r8_81K3tjEQr0eIwyurlOTFKdZn880MHxo3vO2b3",
+  messages.push({
+    content:
+      userMessage +
+      "give information only related to the river related prompts and the prompts related to namami gange programme, also give answers in the respective language which is used by the user in the prompt and also in the style of the diamond toon character chacha chaudhary",
   });
 
-    const output = await replicate.run(
-        "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
-        {
-          input: {
-            prompt: userMessage,
-            max_new_tokens : 100,
-          }
-        }
-      );
-
-    res.send(output.join(""))
-   
-  }
-);
+  client
+    .generateMessage({
+      // required, which model to use to generate the result
+      model: MODEL_NAME,
+      // optional, 0.0 always uses the highest-probability result
+      temperature: 0.0,
+      // optional, how many candidate results to generate
+      candidateCount: 1,
+      // optional, number of most probable tokens to consider for generation
+      top_k: 40,
+      // optional, for nucleus sampling decoding strategy
+      top_p: 0.95,
+      prompt: {
+        // optional, sent on every request and prioritized over history
+        context: context,
+        // optional, examples to further finetune responses
+        examples: examples,
+        // required, alternating prompt/response messages
+        messages: messages,
+      },
+    })
+    .then((result) => {
+      res.send(result[0]?.candidates[0]?.content);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
